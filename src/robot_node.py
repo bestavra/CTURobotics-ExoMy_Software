3888d76839b4d2a38baf8dabc11860b13f4394eb
0xD0M1M0 <76812428+0xD0M1M0@users.noreply.github.com>
2021-03-31 17:54:18 +0200

    Apply range node to robot node and autostart

--- src/robot_node.py
+++ src/robot_node.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import time
-from exomy.msg import RoverCommand, MotorCommands, Screen
+from exomy.msg import RoverCommand, MotorCommands
+from sensor_msgs.msg import Range
 import rospy
+import numpy as np
 from rover import Rover
 import message_filters
 
@@ -9,10 +11,21 @@ import message_filters
 global exomy
 exomy = Rover()
 
+global range
+range = 0
 
 def joy_callback(message):
     cmds = MotorCommands()
-
+    
+    global range
+    min_safety_range = 19
+    max_safety_range = 45
+    if range > -1 and range < max_safety_range and message.steering > 0 and message.locomotion_mode == 1:
+        calc_range = np.clip(range,min_safety_range,max_safety_range)
+        speed_factor = (max_safety_range - min_safety_range) * ( calc_range - min_safety_range) / 100
+        message.vel = message.vel * speed_factor
+        rospy.loginfo("Approching object...slowing down!")
+    
     if message.motors_enabled is True:
         exomy.setLocomotionMode(message.locomotion_mode)
 
@@ -26,6 +39,10 @@ def joy_callback(message):
 
     robot_pub.publish(cmds)
 
+def range_publisher(data):
+    global range
+    range = round(data.range*100,0)
+    return
 
 if __name__ == '__main__':
     rospy.init_node('robot_node')
@@ -33,7 +50,10 @@ if __name__ == '__main__':
     global robot_pub
     joy_sub = rospy.Subscriber(
         "/rover_command", RoverCommand, joy_callback, queue_size=1)
-
+    range_sub = rospy.Subscriber(
+        "/range", Range, range_publisher, queue_size=1)
+    
+    
     rate = rospy.Rate(10)
 
     robot_pub = rospy.Publisher("/motor_commands", MotorCommands, queue_size=1)
